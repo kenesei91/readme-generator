a62157a084a122b2ce8510c2bb20f12ada5d5dcf
Ken Ilo <keneilo91@yahoo.com>
2021-10-02 23:27:02 -0700

    question generates & generate markdown changes to conditions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license) {
+  if (license !== 'no license') {
     return `
     ![badge](https://img.shields.io/badge/License-${license}-red.svg)
     `;
@@ -13,7 +13,7 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license) {
+  if (license !== 'no license') {
     return `
     [${license}](https://choosealicense.com/licenses/${license})
     `;
@@ -25,7 +25,7 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license) {
+  if (license !== 'no license') {
     return `
     ## [License](#table-of-contents)
     
