96a3191abe56943fd8fdd0a12fb3fca2191ea4ab
Ken Ilo <keneilo91@yahoo.com>
2021-10-02 23:15:52 -0700

    packages included for application

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,22 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-
-
 /*fs.writeFile('write.txt', 'goals are the best for us', function(err) {
     if (err) return console.log(err);   
 })
 console.log('success');*/
 
+
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
