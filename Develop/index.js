75bbdefdd949030a2711b4a876bf1d3281cc0bf2
Ken Ilo <keneilo91@yahoo.com>
2021-10-02 23:04:28 -0700

    generate markdown

---
+++ Develop/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+
+/*fs.writeFile('write.txt', 'goals are the best for us', function(err) {
+    if (err) return console.log(err);   
+})
+console.log('success');*/
+
